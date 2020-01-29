bd1f230933ab738ced154f2a039d2b9fc98d125b
Xikmatilla44 <xikmatilla327@gmail.com>
2020-01-29 17:59:07 +0500

    Updates

---
+++ precache-manifest.1f98232aa3aba03093145ac7eaab0a6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7a3b8709d6c7abd9b522f0f07c2b762",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6b96418570fcc60feba2",
+    "url": "/monsters-rolodex/static/css/main.349f2566.chunk.css"
+  },
+  {
+    "revision": "deb290ec9935d96a2afa",
+    "url": "/monsters-rolodex/static/js/2.c55e4a66.chunk.js"
+  },
+  {
+    "revision": "6b96418570fcc60feba2",
+    "url": "/monsters-rolodex/static/js/main.5282991e.chunk.js"
+  },
+  {
+    "revision": "5cffc8a8bbe97e4d15f0",
+    "url": "/monsters-rolodex/static/js/runtime-main.a0087857.js"
+  }
+]);
